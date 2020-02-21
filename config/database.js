149db361d1ed147633d997311cd82801b068088d
Brennan Manion <brennanmanion@Brennans-MBP.hsd1.co.comcast.net>
2020-02-21 01:34:34 -0700

    modified the mysql object in database.js instead of the top connection object

--- config/database.js
+++ config/database.js
@@ -18,13 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
- connection: {
-  host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-  port: Env.get('DB_PORT', ''),
-  user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-  password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-  database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-},
+  connection: Env.get('DB_CONNECTION', 'sqlite'),
 
   /*
   |--------------------------------------------------------------------------
@@ -58,12 +52,12 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+    } 
   },
 
   /*
